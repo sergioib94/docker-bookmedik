6254eb10f948e906c43d9b92018c3eeecd500772
root <root@debian>
2021-02-26 12:19:50 +0100

    ejercicio1 funcionando

---
+++ build/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sed -i "s/$this->user=\"root\";/$this->user=\"$MARIADB_USER\";/g" /var/www/html/core/controller/Database.php
+sed -i "s/$this->pass=\"\";/$this->pass=\"$MARIADB_PASS\";/g" /var/www/html/core/controller/Database.php
+sed -i "s/$this->host=\"localhost\";/$this->host=\"$MARIADB_HOST\";/g" /var/www/html/core/controller/Database.php
+sed -i "s/$this->ddbb=\"bookmedik\";/$this->ddbb=\"$DB_NAME\";/g" /var/www/html/core/controller/Database.php
+
+apache2ctl -D FOREGROUND
